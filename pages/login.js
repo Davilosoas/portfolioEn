63225fa4426469ea2e69e61e61a170e5482dcdd4
Davilosoas <davilosoas@gmail.com>
2022-04-12 13:44:41 -0300

    main

--- pages/login.js
+++ pages/login.js
@@ -1,47 +1,48 @@
 function login() {
     return(
         <html>
-            <body> 
-                <main>
-                    <form action="" method="post">
-                            <h1>
-                                Welcome Back
-                            </h1>
-                            <section>
-                                <label for="name" class="lbl">
-                                    Username*
-                                </label>
-                                <input type="text" placeholder="Enter your username here." class="ipt"></input>
-                            </section>
-                            <section>
-                                <label for="psw" class="lbl">
-                                    Password*
-                                </label>
-                                <input type="text" placeholder="Enter your password here." class="ipt"></input>
-                            </section>
-                            <button type="checkbox" class="check">
-                            
-                            </button>
-                            <span class="checkLbl">
-                                remember me
-                            </span>
-                            <div class="center">
-                                <button class="lgnBtn">
-                                    LOGIN
-                                </button>
-                            </div>
-                            <section class="links">
-                                <a href="/register.html" class="linkRgst">
-                                    Don't have an account?
-                                </a>
-                                <a href="/changePsw.html" class="linkPsw">
-                                    Forgot your password?
-                                </a>
-                            </section>
-                        
-                    </form>
-                </main>
-            </body>
+            <body class="lgnBody"> 
+    <main class="lgnMain">
+        <form action="" method="post">
+                <h1 class="lgnH1">
+                    Welcome Back
+                </h1>
+                <section>
+                    <label for="name" class="lbl">
+                        Username*
+                    </label>
+                    <input type="text" placeholder="Enter your username here." class="ipt"></input>
+                </section>
+                <section>
+                    <label for="psw" class="lbl">
+                        Password*
+                    </label>
+                    <input type="text" placeholder="Enter your password here." class="ipt"></input>
+                </section>
+                <button type="checkbox" class="check">
+                
+                </button>
+                <span class="checkLbl">
+                    remember me
+                </span>
+                <div class="center">
+                    <button class="lgnBtn">
+                        LOGIN
+                    </button>
+                </div>
+                <section class="links">
+                    <a href="/register.html" class="linkRgst">
+                        Don't have an account?
+                    </a>
+                    <a href="/changePsw.html" class="linkPsw">
+                        Forgot your password?
+                    </a>
+                </section>
+            
+        </form>
+    </main>
+</body>
+
 
 
 
