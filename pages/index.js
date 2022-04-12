fc37d0f3446296c86febd25907257e4daec24427
Davilosoas <davilosoas@gmail.com>
2022-04-11 22:10:51 -0300

    main

--- pages/index.js
+++ pages/index.js
@@ -131,16 +131,19 @@ function home() {
       </main>
       
       <footer class="text-muted py-5">
-        <div class="container">
-          <p class="float-end mb-1">
-            <a href="#">Back to top</a>
-          </p>
-          <a href = "https://www.linkedin.com/in/davils/">
+        <div class="container flex">
+          
+          <a href = "https://www.linkedin.com/in/davils/" class="mr-4">
             <img src ="https://blog-assets.hootsuite.com/wp-content/uploads/2018/09/In-2C-54px-R.png"></img>
           </a>
           <a href = "mailto:davilosoas@gmail.com">
-            <img class = "mr-10" src ="https://ucdfbced92a31ac5c4c93da4095e.previews.dropboxusercontent.com/p/thumb/ABhI6su9JE845F3TUPt-2ZOZwafbPLswbvDcvhryf_nfyg7id2W56Rx76hN1LI8rYci-9JnHSUkYncppQQVjt47O8KBdt6aVxV3-Oor1qKdC7t3qDaqU_1WKN0BUk2bPkbrZ0Nv281MhG9vqI1HmiaGJSccCZas1xDcg_wejAEdstvfQMPldWXalFyh792by2adxKqB9fKexDrcMPUAdhxOKVepxKOt9vV2ZOyqevk9RNKYaXU8MTGm7sUJ0F7WNqjD7muHW2anNwYcd9gkp6zr5UtNwmZ1UIy2-7AFyQlUDT2kiM4WVoATuZCAaIM1sU2cf4d8BQr0ZcBureTduX7SO8rH_lAdOxjK_bb2GVw6hswX_xw-KKeK3dVoLx5P35NYerJ6gjAV5q9508UtVcWSLhw0duRp9QFkhZcSMTYWn-A/p.png"></img>
+            <img class = "" src ="https://ucdfbced92a31ac5c4c93da4095e.previews.dropboxusercontent.com/p/thumb/ABhI6su9JE845F3TUPt-2ZOZwafbPLswbvDcvhryf_nfyg7id2W56Rx76hN1LI8rYci-9JnHSUkYncppQQVjt47O8KBdt6aVxV3-Oor1qKdC7t3qDaqU_1WKN0BUk2bPkbrZ0Nv281MhG9vqI1HmiaGJSccCZas1xDcg_wejAEdstvfQMPldWXalFyh792by2adxKqB9fKexDrcMPUAdhxOKVepxKOt9vV2ZOyqevk9RNKYaXU8MTGm7sUJ0F7WNqjD7muHW2anNwYcd9gkp6zr5UtNwmZ1UIy2-7AFyQlUDT2kiM4WVoATuZCAaIM1sU2cf4d8BQr0ZcBureTduX7SO8rH_lAdOxjK_bb2GVw6hswX_xw-KKeK3dVoLx5P35NYerJ6gjAV5q9508UtVcWSLhw0duRp9QFkhZcSMTYWn-A/p.png"></img>
           </a>
+          <p class="float-end flex mb-1 w-screen justify-end">
+            <a href="#">
+              <img class="items-center" src="https://previews.dropbox.com/p/thumb/ABhR3M3Qi6LSmEPkFSSOUwKg4l1IVkjaibgMU05s_VdS6RRmm7eQwELXa3INTFY9mva6CF6Qb_WmFQk5Tp5CMIk44GaxT8F2g2IH_UbbuhXvOOGs80BwuPJIufqG4uxa_bFz8T_b7XiblZXKarXSaQjfz63hYDAZWckaDTruiCgxAK1seinchGWo9Rm8GkjHf9qYpUyyLoqknSz4W1oyy6k9rBhuBmIxICnY_VRfabyMEHLEXj3wG5Hb4BaTtosVggwcf1SRXGpmrcs9Ohg_ye8fJIscpQ0iC7aDDmJAyqsU7n5J41Bp7cTx6GNN1KPXuiA0ylgh11ADFkytB8W00ZthUmSraauar06Fn-00_sgaALHccCAaRnZOO_2GF_N3npA/p.png"></img>
+            </a>
+          </p>
         </div>
       </footer>
       
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./pages/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
