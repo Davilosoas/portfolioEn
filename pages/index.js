21fc7978539737d365d44e60da808069a2112c6e
Davilosoas <davilosoas@gmail.com>
2022-04-12 21:06:58 -0300

    main

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ function home() {
         <section class="py-5 text-center container">
           <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
-              <h1 class="fw-light">
+              <h1 class=" font-medium">
                 Welcome to my portfolio
                 </h1>
               <p class="lead text-muted">
